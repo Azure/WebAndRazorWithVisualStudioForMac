3a24950ba83de9752af9c82b669f007cfb8f1d2c
bradygmsft <bradyg@microsoft.com>
2018-09-12 12:53:04 -0700

    initial creation

---
+++ RazorAndFunction.Function/RazorAndFunction.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+    
+
+namespace RazorAndFunction.Function
+{
+    public static class RazorAndFunction
+    {
+        [FunctionName("RazorAndFunction")]
+        public static async void Run([BlobTrigger("uploads/{name}")]Stream myBlob, 
+                               string name, TraceWriter log, ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            ComputerVisionClient client = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(config["computerVisionKey"]),
+                new System.Net.Http.DelegatingHandler[] { });
+
+
+            client.Endpoint = config["computerVisionEndpoint"];
+
+            try
+            {
+                var result = await client.AnalyzeImageInStreamAsync(myBlob, features);
+                DisplayResults(result, log, name);
+
+                Debug.WriteLine(result);
+            }
+            catch(Exception x)
+            {
+                Debug.WriteLine(x);
+            }
+        }
+
+        private static void DisplayResults(ImageAnalysis result, TraceWriter log, string filename)
+        {
+            if (!(result.Description != null
+                && result.Description.Captions != null
+                && result.Description.Captions.Count > 0 
+                && result.Tags != null
+                  && result.Tags.Count > 0))
+            {
+                return;
+            }
+
+            log.Info("---------------------------------------------------------");
+            log.Info("Image Uploaded - Filename is " + filename);
+            log.Info("---------------------------------------------------------");
+
+
+            if (result.Description != null 
+               && result.Description.Captions != null 
+               && result.Description.Captions.Count > 0)
+            {
+                log.Info("Image Captions:");
+
+                foreach (var caption in result.Description.Captions)
+                {
+                    log.Info(caption.Text + " (" + caption.Confidence + ")");
+                }
+            }
+
+            foreach (var tag in result.Tags)
+            {
+                log.Info(tag.Name + " (" + tag.Confidence + ")");
+            }
+        }
+
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Description,
+            VisualFeatureTypes.Tags
+        };
+    }
+}
---
+++ RazorAndFunction.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace RazorAndFunction.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
+        private IHostingEnvironment _environment;
+        private IConfiguration _configuration;
+
+        public IndexModel(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            _environment = environment;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPostAsync()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                // todo: read from configuration
+                string storageConnectionString = _configuration.GetConnectionString("StorageAccountConnectionString");
+
+                CloudStorageAccount account = CloudStorageAccount.Parse(storageConnectionString);
+                CloudBlobClient serviceClient = account.CreateCloudBlobClient();
+
+                // Create container. Name must be lower case.
+                Console.WriteLine("Creating container...");
+                var container = serviceClient.GetContainerReference("uploads");
+                container.CreateIfNotExistsAsync().Wait();
+
+                // write a blob to the container
+                CloudBlockBlob blob = container.GetBlockBlobReference(Upload.FileName);
+                Upload.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+                await blob.UploadFromStreamAsync(memoryStream);
+            }
+        }
+    }
+}
